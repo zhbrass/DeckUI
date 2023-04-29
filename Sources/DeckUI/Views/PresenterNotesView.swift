9704b63d1466ad85330887c9ca6dbd45eb8cb978
zhbrass <zhbrass@gmail.com>
2023-04-29 15:20:48 -0500

    Beginnings of an adjustable font size presenter view

--- Sources/DeckUI/Views/PresenterNotesView.swift
+++ Sources/DeckUI/Views/PresenterNotesView.swift
@@ -8,13 +8,61 @@
 import SwiftUI
 public struct PresenterNotesView: View {
     @ObservedObject private var presentationState = PresentationState.shared
-    
+    @State private var fontSize: CGFloat = 40
     public var body: some View {
-        Group{
+        VStack(alignment:.leading){
+            Spacer()
             Text(presentationState.deck.slides()[presentationState.slideIndex].comment ?? "No notes")
+                .font(.system(size: fontSize))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(20)
+
+                
+            Spacer()
+            
+//            #if canImport(UIKit)
+            HStack{
+                Spacer()
+                
+                Button{
+                    fontSize -= 5
+                } label : {
+                    Text("A")
+                }.font(.system(size: 30))
+
+                Button{
+                    fontSize += 5
+                } label : {
+                    Text("A")
+                }.font(.system(size: 40))
+
+            }.buttonStyle(.plain)
+                .padding(20)
+//            #endif
+            
 
         }.frame(maxWidth:.infinity, maxHeight: .infinity)
             .background(.background)
+//            .toolbar {
+//                ToolbarItem {
+//                    Button{
+//                        fontSize -= 5
+//                    } label : {
+//                        Text("A")
+//                            .font(.system(size: 15))
+//                    }
+//                }
+//                ToolbarItem {
+//
+//                    Button{
+//                        fontSize += 5
+//                    } label : {
+//                        Text("A")
+//                            .font(.system(size: 20))
+//                    }
+//                }
+//            }
+
         #if canImport(UIKit)
         .slideNavigationGestures()
         #endif
